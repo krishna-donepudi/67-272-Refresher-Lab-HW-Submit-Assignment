89730b8fa9140b8c8d42df4fcb014418e274f1e0
Krishna Donepudi <kdonepud@andrew.cmu.edu>
2021-02-07 17:21:19 -0500

    Passing all but one test for Child Model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,18 @@
 class Child < ApplicationRecord
-    
+  # Relationships
+  has_many :chores
+  has_many :tasks, through: :chores
+
+  # Validations
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  # Methods
+  def name
+    first_name + " " + last_name
+  end
+
+  # Scopes
+  scope :active,       -> { where(active: true) }
+  scope :alphabetical, -> { order('first_name, last_name') } 
 end
